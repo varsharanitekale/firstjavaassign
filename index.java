ca33dd22bc46354cabb0ecb89bc24f725ea5b98d
varsharanitekale <varsharanitekale50@gmail.com>
2021-11-10 12:08:06 +0530

    initial first

---
+++ index.java
@@ -0,0 +1,85 @@
+//  class index{
+     
+//     public static void main(String[] args){
+//       System.out.print("hello world \n");
+    
+//       System.out.println("hii, \n i am fine");
+      
+      
+      
+//      }
+//  }
+//--------------second program--------------------------
+// class basic{
+//     public static void main(String[] args){
+//         System.out.println("*");
+//         System.out.println("* *");
+//         System.out.println("* * *");
+//         System.out.println("* * * *");
+//         System.out.println("* * * * * ");
+   
+//}
+//}
+  //--------------------which  one is greater--------
+
+// class intro{
+//     public static void main(String[] args){
+//         int a= 40;
+//         int b= 20;
+//         if(a>b){
+//             System.out.println("a is greater" + a);
+
+//         }
+//          else if(b>a){
+//             System.out.println("b is greater");
+
+//         }
+//         else{
+//             System.out.println(" both are equal");
+//         }
+
+
+//     }
+// }
+
+
+//--------------which one is smaller----------
+// class smaller{
+//     public static void main(String[] args){
+//     int p=10;
+//     int r=40;
+//     if(p<r){
+//         System.out.println("a is smaller");
+
+//     }
+//     else if(p>r){
+//         System.out.println(" b is smaller");
+
+//     }
+//     else{
+//         System.out.println("both are equal");
+//     }
+// }
+// }
+//------------------------------------for loop------------
+
+
+class loop{
+    public static void main(String[] args){
+        //  for( int i=1; i<=10; i++){
+        //     System.out.println(i);
+        //  }
+        // for(int j=10; j>=1; j--){
+        //     System.out.println(j);
+        // }
+
+        for(int k = 0; k<=10; k=k+2){
+            System.out.println(k);
+        }
+    }
+
+    
+     
+
+}
+
---
+++ second.java
@@ -0,0 +1,13 @@
+public class second {
+    public static void main(String[] args){
+        // for(int k=0; k<=10; k=k+2){
+        //     System.out.println(k);
+        // }
+        int k=0;
+        while(k<=10){
+            System.out.println(k);
+            k=k+2;
+        }
+    }
+    
+}
